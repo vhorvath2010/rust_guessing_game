185979eb39c36a201efc448e8f23d2db3e84a6cf
Vince <vhorvath2010@gmail.com>
2024-07-30 23:49:38 -0400

    allow multple guesses

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,29 @@ fn main() {
 
     let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
 
-    println!("Input your guess:");
+    loop {
+        println!("Input your guess (1-100):");
 
-    let mut guess: String = String::new();
+        let mut guess: String = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
